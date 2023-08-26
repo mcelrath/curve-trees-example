6a5be6829df526dd606fd850f3b0c1bd10d1abea
Amr Ali <amralicc@gmail.com>
2023-08-26 21:39:12 +0300

    Cleanup example code

--- main.rs
+++ main.rs
@@ -1,66 +1,81 @@
-#[macro_use]
-extern crate bulletproofs;
-extern crate relations;
-
+use ark_ec::short_weierstrass::Affine;
+use ark_ff::Zero;
+use ark_secp256k1::{Config as SecpConfig, Fr as SecpScalarBase};
+use ark_secq256k1::Config as SecqConfig;
+use ark_std::UniformRand;
+use rand::thread_rng;
 use relations::curve_tree::*;
 
-use rand::thread_rng;
-
-use ark_ff::PrimeField;
-use ark_std::{UniformRand, Zero};
-use ark_ec::{
-    short_weierstrass::{Affine, SWCurveConfig},
-    CurveGroup
-};
-use ark_secp256k1::{Config as SecpConfig, Fq as SecpFieldBase, Fr as SecpScalarBase};
-use ark_secq256k1::{Config as SecqConfig};
-
-// A simple set of values to add to the accumulator
-const VALUES: [&str; 5] = ["foo", "bar", "baz", "qux", "quux"];
+const NUM_ELEMENTS: usize = 5;
+const DEPTH: usize = 3;
+const LEAF_WIDTH: usize = 5;
+const GEN_LOG_2: usize = 11;
 
 fn main() {
-    let mut rng = rand::thread_rng();
-    let depth = 3;
-    let leaf_width = 5;
-    let generators_length_log_2 = 11;
-    let generators_length = 1 << generators_length_log_2;
+    let mut rng = thread_rng();
 
-    let sr_params = SelRerandParameters::<SecpConfig, SecqConfig>::new(generators_length, generators_length, &mut rng);
+    // Generate random leaf elements
+    let leaf_elements = generate_random_leaf_elements(&mut rng);
+    display_elements("Leaf elements:", &leaf_elements);
 
-    const NELEMENTS: usize = 5;
+    // Create select-rerandomize reltion and generate leaf commitments
+    let sr_params = create_sel_rerand_parameters(&mut rng);
+    let leaf_commitments = create_leaf_commitments(&leaf_elements, &sr_params);
+    display_elements("Leaf Commitments:", &leaf_commitments);
 
-    // So far this example generates random curve points instead of using VALUES.
-    let leaf_elements: Vec<_> = (0..leaf_width)
-        .map(|_| SecpScalarBase::rand(&mut rng))
-        .collect();
-    println!("Leaf elements:");
-    for i in 0..NELEMENTS {
-        println!("\t{}", leaf_elements[i]);
-    }
+    // Create permissible points and randomness
+    let (permissible_points, _permissible_randomnesses) =
+        create_permissible_points_and_randomnesses(&leaf_commitments, &sr_params);
+    display_elements("Set elements:", &permissible_points);
 
-    let leaf_commitments: Vec<_> = (0..leaf_width)
-        .map(|x| {sr_params
-            .even_parameters
-            .commit(&[leaf_elements[x]], SecpScalarBase::zero(), 0)
-        }).collect();
-    println!("Leaf Commitments:");
-    for i in 0..NELEMENTS {
-        println!("\t{}", leaf_commitments[i]);
-    }
+    let _curve_tree = CurveTree::<{ NUM_ELEMENTS }, SecpConfig, SecqConfig>::from_set(
+        &permissible_points,
+        &sr_params,
+        Some(DEPTH),
+    );
+}
 
-    let (permissible_points, permissible_randomnesses): (Vec<_>, Vec<_>) = (0..leaf_width)
-        .map(|x| { sr_params.even_parameters.uh.permissible_commitment(
-            &leaf_commitments[x],
-            &sr_params.even_parameters.pc_gens.B_blinding)
-        }).unzip();
+fn create_sel_rerand_parameters<R: rand::Rng>(
+    rng: &mut R,
+) -> SelRerandParameters<SecpConfig, SecqConfig> {
+    let generators_length = 1 << GEN_LOG_2;
+    SelRerandParameters::<SecpConfig, SecqConfig>::new(generators_length, generators_length, rng)
+}
 
-    println!("Committed set size: {}", permissible_points.len());
-    println!("Set elements:");
-    for i in 0..permissible_points.len() {
-        println!("\t{}", permissible_points[i]);
-    }
+fn generate_random_leaf_elements<R: rand::Rng>(rng: &mut R) -> Vec<SecpScalarBase> {
+    (0..LEAF_WIDTH).map(|_| SecpScalarBase::rand(rng)).collect()
+}
 
-    let curve_tree = CurveTree::<{NELEMENTS}, SecpConfig, SecqConfig>::from_set(&permissible_points, &sr_params, Some(depth));
+fn create_leaf_commitments(
+    leaf_elements: &[SecpScalarBase],
+    sr_params: &SelRerandParameters<SecpConfig, SecqConfig>,
+) -> Vec<Affine<SecpConfig>> {
+    leaf_elements
+        .iter()
+        .map(|&elem| {
+            sr_params
+                .even_parameters
+                .commit(&[elem], SecpScalarBase::zero(), 0)
+        })
+        .collect()
+}
 
-    // FIXME now output witnesses and make proofs...
+fn create_permissible_points_and_randomnesses(
+    leaf_commitments: &[Affine<SecpConfig>],
+    sr_params: &SelRerandParameters<SecpConfig, SecqConfig>,
+) -> (Vec<Affine<SecpConfig>>, Vec<SecpScalarBase>) {
+    leaf_commitments
+        .iter()
+        .map(|commitment| {
+            sr_params
+                .even_parameters
+                .uh
+                .permissible_commitment(commitment, &sr_params.even_parameters.pc_gens.B_blinding)
+        })
+        .unzip()
+}
+
+fn display_elements<T: std::fmt::Display>(header: &str, elements: &[T]) {
+    println!("{}", header);
+    elements.iter().for_each(|elem| println!("\t{}", elem));
 }
